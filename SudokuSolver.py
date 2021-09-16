e6127af57a395bd6bf91739d022bad9225714dad
Isaac <ischmidt707@gmail.com>
2021-09-16 14:55:25 -0600

    Create SudokuSolver.py
    Quick draft of program layout

---
+++ SudokuSolver.py
@@ -0,0 +1,36 @@
+"""
+Project 1 for CSCI 446
+
+Isaac Schmidt and Nic Dzomba
+
+Description: Testing implementations of 5 different AI algorithms for
+solving sudoku puzzles of a range of difficulties.
+"""
+
+class ConstraintSolver:
+    pass
+
+class BacktrackSimple(ConstraintSolver):
+    pass
+
+class BacktrackFWCheck(ConstraintSolver):
+    pass
+
+class BacktrackArcCons(ConstraintSolver):
+    pass
+
+class LocalSearchSAMC(ConstraintSolver):
+    pass
+
+class LocalSearchGenetic(ConstraintSolver):
+    pass
+
+class Puzzle:
+    def __init__(self, board):
+        self.board = []
+
+    def import():
+        pass
+
+class Main:
+    pass
