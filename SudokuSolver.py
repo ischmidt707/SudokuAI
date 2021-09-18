d5ccf85252a7c785d50323ac69531b342ca9edd6
nic-dzomba <nicustm@gmail.com>
2021-09-18 15:00:24 -0600

    Update SudokuSolver.py
    implement constraintcheck in constraintsolver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -9,42 +9,73 @@ solving sudoku puzzles of a range of difficulties.
 
 import numpy as np
 import csv
+import copy
 
 
 class ConstraintSolver:
-    def __init__(self):
+    def __init__(self, puzzle):
         self.operations = 0
+        # create copy of puzzle object with new pointer to ensure puzzle from main not changed
+        self.puzzle = copy.deepcopy(puzzle)
 
+    # count number of operations to provide hardware independent evaluation of algorithm efficiency
     def count(self):
         self.operations += 1
 
     def solve(self):
         pass
 
+    # check if all constraints of the puzzle are met with new value added
+    def constraintCheck(self, x, y, val):
+        # check if there are duplicates in row
+        for i in range(0, 9):
+            if (self.puzzle.board[x, i] == val):
+                return False
+        # check if there are duplicates in column
+        for i in range(0, 9):
+            if (self.puzzle.board[i, y] == val):
+                return False
+        # check if there are duplicates in box
+        xb = x - x % 3
+        yb = y - y % 3
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if (self.puzzle.board[xb + i][yb + j] == val):
+                    return False
+        # all constraints pass
+        return True
 
+
+# all specific solvers have constraintsolver as superclass
+
+# simple backtracking
 class BacktrackSimple(ConstraintSolver):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, puzzle):
+        super().__init__(puzzle)
 
 
+# backtracking with forward checking
 class BacktrackFWCheck(ConstraintSolver):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, puzzle):
+        super().__init__(puzzle)
 
 
+# backtracking with arc consistentcy
 class BacktrackArcCons(ConstraintSolver):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, puzzle):
+        super().__init__(puzzle)
 
 
+# simulated annealing with miinum conflict heuristic
 class LocalSearchSAMC(ConstraintSolver):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, puzzle):
+        super().__init__(puzzle)
 
 
+# genetic algorithm w/ penalty function and tournament selection
 class LocalSearchGenetic(ConstraintSolver):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, puzzle):
+        super().__init__(puzzle)
 
 
 # puzzle class used to input and store the puzzles being solved
